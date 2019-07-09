49e012655f3a0a43de17b26e24818553e0f14362
John Lalor <john.lalor@nd.edu>
2019-07-09 13:51:19 -0400

    update scoring function

--- src/features/irt_scoring.py
+++ src/features/irt_scoring.py
@@ -15,6 +15,8 @@ def theta_fn(difficulties, student_prior, response_pattern):
         #print(probabilities) 
         log_likelihood = student_prior.logpdf(theta) 
         for i, rp in enumerate(response_pattern):
+            if rp == 0:
+                rp == -1
             log_likelihood += np.log1p((2 * probabilities[i] - 1) * rp) 
         #print(log_likelihood)
         return  -log_likelihood 
@@ -28,13 +30,15 @@ def calculate_theta(difficulties, response_pattern):
 
     student_prior = norm(loc=0., scale=1.) 
     fn = theta_fn(difficulties, student_prior, response_pattern) 
-    result = minimize(fn, [-2], method='Nelder-Mead') 
+    result = minimize(fn, [0.1], method='Nelder-Mead') 
     return result['x']
 
 
 def test():
     diffs = [-3., -2., 2., 3.]
-    rp = [1,1,1,1]
-
+    diffs = [-1.0, -0.75, 0.02, 1.4, 3.2]
+    rp = [-1, -1, -1, -1, -1]
+    #rp = [0,0,0,0,0]
     print(calculate_theta(diffs, rp)) 
 
+#test() 
