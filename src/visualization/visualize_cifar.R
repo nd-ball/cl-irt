e289e6426f5a3a69d59b27685b0770c758310342
John Lalor <john.lalor@nd.edu>
2019-07-11 14:49:47 -0400

    more work on aaai sub

--- src/visualization/visualize_cifar.R
+++ src/visualization/visualize_cifar.R
@@ -1,24 +1,40 @@
 library(tidyverse)
 
 data_dir <- 'G:/My Drive/data/curriculum_learning_irt/'
-D.baseline <- read_csv(paste(data_dir,'cifar_baseline.log',sep=''), 
-              col_names = c('train_size', 'train_acc', 'test_loss', 'test_acc'),
-              skip=2, n_max=20) 
-D.baseline$epoch <- c(1:20) 
+exp_type <- 'sstb'
+num_skip <- 18
+D.baseline <- read_csv(paste(data_dir, exp_type,'_baseline_easiest.log',sep=''), 
+              col_names = c('a','b','train_size', 'train_acc', 'test_loss', 'test_acc', 'theta'),
+              skip=num_skip, n_max=100) 
+D.baseline$epoch <- c(1:100) 
 D.baseline$exp <- 'baseline'
 
+D.irt <- read_csv(paste(data_dir, 'irt_cl_', exp_type, '.log', sep=''),
+                  col_names = c('a','b','train_size', 'train_acc', 'test_loss', 'test_acc', 'theta'),
+                  skip=num_skip, n_max=100)
+D.irt$epoch <- c(1:100)
+D.irt$exp <- 'irt'
 
-D.easiest <- read_csv(paste(data_dir,'cifar_cl_simple_not_balanced-easiest.log',sep=''), 
-                       col_names = c('train_size', 'train_acc', 'test_loss', 'test_acc'),
-                       skip=2, n_max=20) 
-D.easiest$epoch <- c(1:20) 
+D.easiest <- read_csv(paste(data_dir,exp_type, '_cl_not_balanced-simple-easiest.log',sep=''), 
+                      col_names = c('a','b','train_size', 'train_acc', 'test_loss', 'test_acc', 'theta'),
+                      skip=num_skip, n_max=100) 
+D.easiest$epoch <- c(1:100) 
 D.easiest$exp <- 'easiest'
 
-D.middleout <- read_csv(paste(data_dir,'cifar_cl_simple_not_balanced-middleout.log',sep=''), 
-                      col_names = c('train_size', 'train_acc', 'test_loss', 'test_acc'),
-                      skip=2, n_max=20) 
-D.middleout$epoch <- c(1:20) 
+D.middleout <- read_csv(paste(data_dir,exp_type, '_cl_not_balanced-simple-middleout.log',sep=''), 
+                        col_names = c('a','b','train_size', 'train_acc', 'test_loss', 'test_acc', 'theta'),
+                        skip=num_skip, n_max=100) 
+D.middleout$epoch <- c(1:100) 
 D.middleout$exp <- 'middleout'
+
+
+
+D <- rbind(D.baseline, D.irt, D.easiest, D.middleout)
+
+ggplot(D, aes(x=epoch, y=test_acc, color=exp))  + 
+  geom_line() + 
+  geom_line(aes(x=epoch, y=train_size/67348, color=exp),D, linetype=2)
+
 
 D.ordered <- read_csv(paste(data_dir,'cifar_cl_ordered_not_balanced-easiest.log',sep=''), 
                       col_names = c('train_size', 'train_acc', 'test_loss', 'test_acc'),
