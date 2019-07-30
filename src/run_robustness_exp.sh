c47c22285fd77909e4691ebecc521ce53dd93cf5
John Lalor <john.lalor@nd.edu>
2019-07-30 13:37:55 -0400

    update script

--- src/run_robustness_exp.sh
+++ src/run_robustness_exp.sh
@@ -31,10 +31,10 @@ do
         for s in simple ordered #balanced 
         do 
             # CL, balanced
-            sbatch -p titanx-long --gres=gpu:1 --mem=90gb --output=logs/robustness/sstb_cl_balanced-$s-$o-$k.log --wrap="python -u -m models.sstb --dynet-autobatch 1 --dynet-gpu 1 --dynet-mem 11000 --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy $s --balanced --ordering $o --num-epochs $NUMEPOCHS"
+            sbatch -p titanx-short --gres=gpu:1 --mem=90gb --output=logs/robustness/sstb_cl_balanced-$s-$o-$k.log --wrap="python -u -m models.sstb --dynet-autobatch 1 --dynet-gpu 1 --dynet-mem 11000 --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy $s --balanced --ordering $o --num-epochs $NUMEPOCHS"
 
             # CL, not balanced
-            sbatch -p titanx-long --gres=gpu:1 --mem=90gb --output=logs/robustness/sstb_cl_not_balanced-$s-$o-$k.log --wrap="python -u -m models.sstb --dynet-autobatch 1 --dynet-gpu 1 --dynet-mem 11000 --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy $s --ordering $o --num-epochs $NUMEPOCHS"
+            sbatch -p titanx-short --gres=gpu:1 --mem=90gb --output=logs/robustness/sstb_cl_not_balanced-$s-$o-$k.log --wrap="python -u -m models.sstb --dynet-autobatch 1 --dynet-gpu 1 --dynet-mem 11000 --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy $s --ordering $o --num-epochs $NUMEPOCHS"
         done 
     done 
 
@@ -61,18 +61,18 @@ do
     ### SSTB
     # random ordered (fixed order across all epochs) 
     # balanced
-    sbatch -p titanx-long --gres=gpu:1 --mem=90gb --output=logs/robustness/sstb_cl_ordered_balanced_hardest_random-$k.log --wrap="python -u -m models.sstb --dynet-autobatch 1 --dynet-gpu 1 --dynet-mem 11000 --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy ordered --balanced --ordering hardest --random --num-epochs $NUMEPOCHS"
+    sbatch -p titanx-short --gres=gpu:1 --mem=90gb --output=logs/robustness/sstb_cl_ordered_balanced_hardest_random-$k.log --wrap="python -u -m models.sstb --dynet-autobatch 1 --dynet-gpu 1 --dynet-mem 11000 --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy ordered --balanced --ordering hardest --random --num-epochs $NUMEPOCHS"
 
     # not balanced
-    sbatch -p titanx-long --gres=gpu:1 --mem=90gb --output=logs/robustness/sstb_cl_ordered_not_balanced_hardest_random-$k.log --wrap="python -u -m models.sstb --dynet-autobatch 1 --dynet-gpu 1 --dynet-mem 11000 --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy ordered --ordering hardest --random --num-epochs $NUMEPOCHS"
+    sbatch -p titanx-short --gres=gpu:1 --mem=90gb --output=logs/robustness/sstb_cl_ordered_not_balanced_hardest_random-$k.log --wrap="python -u -m models.sstb --dynet-autobatch 1 --dynet-gpu 1 --dynet-mem 11000 --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy ordered --ordering hardest --random --num-epochs $NUMEPOCHS"
 
 
     # random simple 
     # balanced
-    sbatch -p titanx-long --gres=gpu:1 --mem=90gb --output=logs/robustness/sstb__cl_simple_balanced_random-$k.log --wrap="python -u -m models.sstb --dynet-autobatch 1 --dynet-gpu 1 --dynet-mem 11000 --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy simple --balanced --ordering hardest --random --num-epochs $NUMEPOCHS"
+    sbatch -p titanx-short --gres=gpu:1 --mem=90gb --output=logs/robustness/sstb__cl_simple_balanced_random-$k.log --wrap="python -u -m models.sstb --dynet-autobatch 1 --dynet-gpu 1 --dynet-mem 11000 --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy simple --balanced --ordering hardest --random --num-epochs $NUMEPOCHS"
 
     # not balanced
-    sbatch -p titanx-long --gres=gpu:1 --mem=90gb --output=logs/robustness/sstb_cl_simple_not_balanced_random-$k.log --wrap="python -u -m models.sstb --dynet-autobatch 1 --dynet-gpu 1 --dynet-mem 11000 --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy simple --ordering hardest --random --num-epochs $NUMEPOCHS" 
+    sbatch -p titanx-short --gres=gpu:1 --mem=90gb --output=logs/robustness/sstb_cl_simple_not_balanced_random-$k.log --wrap="python -u -m models.sstb --dynet-autobatch 1 --dynet-gpu 1 --dynet-mem 11000 --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy simple --ordering hardest --random --num-epochs $NUMEPOCHS" 
 
     # irt CL
 
