e7a1998a6c904b358f86ff97d64ce91de5f0a0e1
John Lalor <john.lalor@nd.edu>
2019-08-05 13:51:46 -0400

    bugfix

--- src/aaai_exp.sh
+++ src/aaai_exp.sh
@@ -10,7 +10,7 @@ do
     sbatch -p m40-long --gres=gpu:1 --mem=90gb --output=logs/$m-baseline.log --wrap="python -u -m models.$m --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --num-epochs $NUMEPOCHS --strategy baseline"
 
     # irt CL
-    #sbatch -p m40-long --gres=gpu:1 --mem=90gb --output=logs/irt-cl-$m-1000.log --wrap="python -u -m models.$m --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --num-epochs $NUMEPOCHS --strategy theta --ordering easiest --min-train-length 1000"
+    sbatch -p m40-long --gres=gpu:1 --mem=90gb --output=logs/irt-cl-$m-1000.log --wrap="python -u -m models.$m --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --num-epochs $NUMEPOCHS --strategy theta --ordering easiest --min-train-length 1000"
 
     # irt CL (hard)
     sbatch -p m40-long --gres=gpu:1 --mem=90gb --output=logs/irt-cl-hard-$m-1000.log --wrap="python -u -m models.$m --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --num-epochs $NUMEPOCHS --strategy theta-hard --ordering hardest --min-train-length 1000"
