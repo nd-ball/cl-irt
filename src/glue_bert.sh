1474e7536ed944f1f9d7da9006a87c05a3f25ef6
John Lalor <john.lalor@nd.edu>
2019-12-06 14:11:56 -0500

    more debugging

--- src/glue_bert.sh
+++ src/glue_bert.sh
@@ -9,23 +9,21 @@ TASK=CoLA
 CACHEDIR=/mnt/nfs/work1/hongyu/lalor/data/bert/
 
 # DDaCLAE
-sbatch -p titanx-long --gres=gpu:1 --mem=64gb --output=logs/bert/bert-cola-ddaclae-test-%j.log --wrap="python -u -m models.glue_ddaclae --gpu 0 --data-dir $DATADIR --strategy theta --min-train-length $MINTRAINLEN --num-epochs $NUMEPOCHS --cache-dir $CACHEDIR --task $TASK --num-obs $NUMOBS --diff-dir $DIFFDIR"
+#sbatch -p titanx-long --gres=gpu:1 --mem=64gb --output=logs/bert/bert-cola-ddaclae-test-%j.log --wrap="python -u -m models.glue_ddaclae --gpu 0 --data-dir $DATADIR --strategy theta --min-train-length $MINTRAINLEN --num-epochs $NUMEPOCHS --cache-dir $CACHEDIR --task $TASK --num-obs $NUMOBS --diff-dir $DIFFDIR"
 
 
 # NAACL Baselines
-#for m in sstb_bert snli_bert 
-#do 
-#    for strat in naacl-linear naacl-root 
-#    do 
-#        for o in easiest hardest  
-#        do 
-#            sbatch -p titanx-long --gres=gpu:1 --mem=64gb --output=logs/bert/bert-$m-$strat-$o-length.log --wrap="python -u -m models.$m --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy $strat --use-length --ordering $o --num-epochs $NUMEPOCHS --cache-dir /mnt/nfs/work1/hongyu/lalor/data/bert/ --competency $COMP"
-#            sbatch -p titanx-long --gres=gpu:1 --mem=64gb --output=logs/bert/bert-$m-$strat-$o-irt.log --wrap="python -u -m models.$m --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy $strat --ordering $o --num-epochs $NUMEPOCHS --cache-dir /mnt/nfs/work1/hongyu/lalor/data/bert/ --competency $COMP" 
-#        done 
-#    done
-#done
+
+ 
+for strat in naacl-linear naacl-root 
+do 
+    for o in easiest hardest  
+    do 
+        sbatch -p titanx-long --gres=gpu:1 --mem=64gb --output=logs/bert/bert-$TASK-$strat-$o-length.log --wrap="python -u -m models.glue_ddaclae --gpu 0 --data-dir $DATADIR --strategy $strat --use-length --ordering $o --num-epochs $NUMEPOCHS --cache-dir $CACHEDIR --competency $COMP --task $TASK --num-obs $NUMOBS --diff-dir $DIFFDIR"
+        sbatch -p titanx-long --gres=gpu:1 --mem=64gb --output=logs/bert/bert-$TASK-$strat-$o-irt.log --wrap="python -u -m models.glue_ddaclae --gpu 0 --data-dir $DATADIR --strategy $strat --ordering $o --num-epochs $NUMEPOCHS --cache-dir $CACHEDIR --competency $COMP --task $TASK --num-obs $NUMOBS --diff-dir $DIFFDIR" 
+    done 
+done
 
 # Fully supervised baselines
-#sbatch -p titanx-long --gres=gpu:1 --mem=64gb --output=logs/bert/bert-sstb-baseline.log --wrap="python -u -m models.sstb_bert --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy baseline --use-length --ordering easiest --num-epochs $NUMEPOCHS --cache-dir /mnt/nfs/work1/hongyu/lalor/data/bert/"
+sbatch -p titanx-long --gres=gpu:1 --mem=64gb --output=logs/bert/bert-$TASK-baseline.log --wrap="python -u -m models.glue_ddaclae --gpu 0 --data-dir $DATADIR --strategy baseline --use-length --ordering $o --num-epochs $NUMEPOCHS --cache-dir $CACHEDIR --competency $COMP --task $TASK --num-obs $NUMOBS --diff-dir $DIFFDIR"
 
-#sbatch -p titanx-long --gres=gpu:1 --mem=64gb --output=logs/bert/bert-snli-baseline.log --wrap="python -u -m models.snli_bert --gpu 0 --data-dir /mnt/nfs/work1/hongyu/lalor/data/cl-data/ --strategy baseline --use-length --ordering easiest --num-epochs $NUMEPOCHS --cache-dir /mnt/nfs/work1/hongyu/lalor/data/bert/"
