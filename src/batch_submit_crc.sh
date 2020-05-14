d733e299df4a9a2a90b956411eb9d20cc3b8b792
John Lalor <john.lalor@nd.edu>
2020-05-14 09:25:18 -0400

    update script

--- src/batch_submit_crc.sh
+++ src/batch_submit_crc.sh
@@ -1,7 +1,8 @@
 
 for n in 1 2 3 4 5
 do
-for TASK in MRPC QNLI RTE QQP MNLI SST-2 
+#for TASK in MRPC QNLI RTE QQP MNLI SST-2 
+for task in SST-2
 do 
     # ddaclae
     qsub glue_test_crc.sh $TASK 
