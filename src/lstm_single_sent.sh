61c3319bf6a5b8b7774a680a0a078c1eb4ec6739
John Lalor <john.lalor@nd.edu>
2020-05-26 13:23:03 -0400

    fix jobs for lstm use length

--- src/lstm_single_sent.sh
+++ src/lstm_single_sent.sh
@@ -20,12 +20,13 @@ STRATEGY=$2
 LENGTH=$3
 CACHEDIR=~/data/bert/
 
-if ["$LENGTH" = "TRUE"]; then
+# FOR NOW (5-26): only run where length is true
+#if ["$LENGTH" = "TRUE"]; then
 python -u -m models.sstb --dynet-autobatch 1 --dynet-gpus 1 --dynet-mem 10000 --gpu 0 --data-dir $DATADIR --strategy $STRATEGY --num-epochs $NUMEPOCHS --diff-dir $DIFFDIR --task $TASK --use-length 
 python -u -m models.sstb --dynet-autobatch 1 --dynet-gpus 1 --dynet-mem 10000 --gpu 0 --data-dir $DATADIR --strategy $STRATEGY --num-epochs $NUMEPOCHS --diff-dir $DIFFDIR --task $TASK --use-length --balanced
-else
-python -u -m models.sstb --dynet-autobatch 1 --dynet-gpus 1 --dynet-mem 10000 --gpu 0 --data-dir $DATADIR --strategy $STRATEGY --num-epochs $NUMEPOCHS --diff-dir $DIFFDIR --task $TASK 
-python -u -m models.sstb --dynet-autobatch 1 --dynet-gpus 1 --dynet-mem 10000 --gpu 0 --data-dir $DATADIR --strategy $STRATEGY --num-epochs $NUMEPOCHS --diff-dir $DIFFDIR --task $TASK --balanced 
-fi
+#else
+#python -u -m models.sstb --dynet-autobatch 1 --dynet-gpus 1 --dynet-mem 10000 --gpu 0 --data-dir $DATADIR --strategy $STRATEGY --num-epochs $NUMEPOCHS --diff-dir $DIFFDIR --task $TASK 
+#python -u -m models.sstb --dynet-autobatch 1 --dynet-gpus 1 --dynet-mem 10000 --gpu 0 --data-dir $DATADIR --strategy $STRATEGY --num-epochs $NUMEPOCHS --diff-dir $DIFFDIR --task $TASK --balanced 
+#fi
 
 echo $TASK-$STRATEGY-$LENGTH 
