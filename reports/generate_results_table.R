ebb8ab3d267c55d74219e25fe3108d698152ffd9
John Lalor <john.lalor@nd.edu>
2020-05-29 09:16:11 -0400

    update table gen for more details

--- reports/generate_results_table.R
+++ reports/generate_results_table.R
@@ -114,7 +114,7 @@ writeResultsTable <- function(modelName){
     outputTable, 
     type = "latex",
     caption = "dev set accuracy results for each task under consideration. During training, 10\\% of the training set was held out and used for early stopping. Highest overall accuracy is bolded. Highest accuracy among competence-based methods is underlined",
-    label = str_glue("tab:acc-{modelName}"),
+    label = str_glue("tab:acc_{modelName}"),
     floating.environment = c("table*", "small", "center")
 )
   
@@ -184,7 +184,7 @@ writeResultsTable <- function(modelName){
     outputTable2, 
     type = "latex",
     caption = "Average epoch of convergence for each model, with 95\\% confidence intervals.",
-    label = str_glue("tab:epoch-{modelName}"),
+    label = str_glue("tab:epoch_{modelName}"),
     floating.environment = c("table*", "small", "center")
   )
   
