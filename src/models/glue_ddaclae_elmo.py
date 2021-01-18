7c964340ddea1ebd3806cfb707c6a26eef3c017d
John Lalor <john.lalor@nd.edu>
2021-01-18 15:10:09 -0500

    bugfix

--- src/models/glue_ddaclae_elmo.py
+++ src/models/glue_ddaclae_elmo.py
@@ -239,6 +239,7 @@ def train(args, outfile):
         for j, batch in enumerate(train_dataloader):
             outputs = model(batch['t1'], batch['t2']) 
             print(outputs.shape)
+            print(batch['t2'])
             loss = criterion(outputs, batch['label'])
             
             loss.backward() 
