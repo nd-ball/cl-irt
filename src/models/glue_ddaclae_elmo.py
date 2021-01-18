6063ae7a2f15c6b6fd3be5aac1371656fbc512c4
John Lalor <john.lalor@nd.edu>
2021-01-18 15:36:49 -0500

    bugfix

--- src/models/glue_ddaclae_elmo.py
+++ src/models/glue_ddaclae_elmo.py
@@ -68,11 +68,12 @@ class CLF(torch.nn.Module):
         )
 
     def forward(self, s1, s2=None):
-        character_ids_s1 = batch_to_ids(s1['tokens']['elmo_tokens'])
+        character_ids_s1 = batch_to_ids(s1)
         s1_emb = self.elmo(character_ids_s1)['elmo_representations'][0]
         if s2 is not None:
-            character_ids_s2 = batch_to_ids(s2['tokens']['elmo_tokens'])
+            character_ids_s2 = batch_to_ids(s2)
             s2_emb = self.elmo(character_ids_s2)['elmo_representations'][0] 
+            print(s1_emb.shape, s2_emb.shape)
             lstm_s1, _ = self.lstm(s1_emb)
             lstm_s2, _ = self.lstm(s2_emb)
             print(lstm_s2.shape)
