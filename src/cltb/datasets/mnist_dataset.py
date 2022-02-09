be1b553c559773cbfcb7faf3886ea25068522dd1
John Lalor <john.lalor@nd.edu>
2022-02-09 16:00:28 -0500

    bugfixing

--- src/cltb/datasets/mnist_dataset.py
+++ src/cltb/datasets/mnist_dataset.py
@@ -7,6 +7,7 @@ from torchvision.datasets.utils import download_url, check_integrity
 import os
 from torchvision import transforms
 import csv
+from PIL import Image
 
 FNAMES = {
     "train": "snli_1.0_train.txt",
@@ -54,14 +55,11 @@ class MNISTDataset(CLDataset):
 
         self.root = os.path.expanduser(self.data_path)
         
-        self.transform = transform=transforms.Compose([
+        self.transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
-        ]),
-        self.target_transform = transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ]),
+        ])
+        self.target_transform = None
         self.train =  self.split == "train" # training set or test set
 
         if not self._check_exists():
@@ -99,25 +97,20 @@ class MNISTDataset(CLDataset):
        
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        examples = self.examples.iloc[idx, 1:]
-        examples = np.array([examples])
-        examples2 = self.examples2.iloc[idx, 1:]
-        examples2 = np.array([examples2])
-        ids = self.ids.iloc[idx, 1:]
-        ids = np.array([ids])
-        #difficulties = self.difficulties.iloc[idx, 1:]
-        difficulties = [self.difficulties[i] for i in ids]
-        difficulties = np.array([difficulties])
-        labels = self.labels.iloc[idx, 1:]
-        labels = np.array([labels])
+        img, target, label, diff = self.data[idx], self.targets[idx], self.idx[idx], self.difficulties[idx]
+        img = Image.fromarray(img.numpy(), mode='L')
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
         sample = {
-            "ids": ids,
-            "examples": examples,
-            "examples2": examples2,  
-            "difficulties": difficulties, 
-            "labels": labels
+            "ids": label,
+            "examples": img,
+            "examples2": None,  
+            "difficulties": diff, 
+            "labels": target
             }
         return sample
 
@@ -145,165 +138,6 @@ class MNISTDataset(CLDataset):
         return img, target, label, diff, pcorrect
 
 
-class my_MNIST(data.Dataset):
-    
-
-    def __init__(self, root, train=True, transform=None, target_transform=None, download=False, diff_dir=None):
-        self.root = os.path.expanduser(root)
-        self.transform = transform
-        self.target_transform = target_transform
-        self.train = train  # training set or test set
-
-        if download:
-            self.download()
-
-        if not self._check_exists():
-            raise RuntimeError('Dataset not found.' +
-                               ' You can use download=True to download it')
-
-        if self.train:
-            data_file = self.training_file
-        else:
-            data_file = self.test_file
-        self.data, self.targets = torch.load(os.path.join(self.root, self.processed_folder, data_file))
-
-        self.idx = torch.tensor(list(range(len(self.data))), dtype=torch.int)
-
-        self.difficulties = []
-        if diff_dir is not None:
-            if self.train:
-                diff_file = diff_dir + 'mnist_diffs_train.csv'
-            else:
-                diff_file = diff_dir + 'mnist_diffs_test.csv' 
-            with open(diff_file, 'r') as infile:
-                diffreader = csv.reader(infile, delimiter=',')
-
-                for row in diffreader:
-                    self.difficulties.append((eval(row[0]), eval(row[1])))
-                self.difficulties_sorted = sorted(self.difficulties, key=lambda x:x[0])
-                self.difficulties = [d[1] for d in self.difficulties_sorted]
-
-            self.difficulties = torch.tensor(self.difficulties, dtype=torch.float)
-        else:
-            self.difficulties = torch.tensor(np.zeros(len(self.data)), dtype=torch.float)
-        
-        if diff_dir is not None:
-            self.pcorrect = []
-            self.pcorrect_dict = {}
-            if self.train:
-                pcorrect_file = diff_dir + 'mnist_rp_train.csv'
-            else:
-                pcorrect_file = diff_dir + 'mnist_rp_test.csv'
-            with open(pcorrect_file, 'r') as infile:
-                pcorrect_reader = csv.reader(infile, delimiter=',')
-
-                # trainsize,noise,itemid,response
-                next(pcorrect_reader)
-                for _, _, itemID, response in pcorrect_reader:
-                    itemID = eval(itemID)
-                    response = eval(response)
-                    if itemID not in self.pcorrect_dict:
-                        self.pcorrect_dict[itemID] = {
-                            'correct': 0.0,
-                            'total': 0.0
-                        }
-                    self.pcorrect_dict[itemID]['correct'] += response
-                    self.pcorrect_dict[itemID]['total'] += 1
-            self.pcorrect = [0.0] * len(self.idx)  
-            for key, val in self.pcorrect_dict.items():
-                self.pcorrect[key] = val['correct'] / val['total']
-            self.pcorrect = torch.tensor(self.pcorrect, dtype=torch.float) 
-        else:
-            self.pcorrect = torch.zeros(len(self.idx), dtype=torch.float)
-       
-
-    def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
-        Returns:
-            tuple: (image, target) where target is index of the target class.
-        """
-        img, target, label, diff = self.data[index], self.targets[index], self.idx[index], self.difficulties[index]
-        pcorrect = self.pcorrect[index] 
-
-        # doing this so that it is consistent with all other datasets
-        # to return a PIL Image
-        img = Image.fromarray(img.numpy(), mode='L')
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-
-
-        return img, target, label, diff, pcorrect
-
-    def __len__(self):
-        return len(self.data)
-
-    def _check_exists(self):
-        return os.path.exists(os.path.join(self.root, self.processed_folder, self.training_file)) and \
-            os.path.exists(os.path.join(self.root, self.processed_folder, self.test_file))
-
-    def download(self):
-        """Download the MNIST data if it doesn't exist in processed_folder already."""
-        import gzip
-
-        if self._check_exists():
-            return
-
-        # download files
-        try:
-            os.makedirs(os.path.join(self.root, self.raw_folder))
-            os.makedirs(os.path.join(self.root, self.processed_folder))
-        except OSError as e:
-            if e.errno == errno.EEXIST:
-                pass
-            else:
-                raise
-
-        for url in self.urls:
-            filename = url.rpartition('/')[2]
-            file_path = os.path.join(self.root, self.raw_folder, filename)
-            download_url(url, root=os.path.join(self.root, self.raw_folder),
-                         filename=filename, md5=None)
-            with open(file_path.replace('.gz', ''), 'wb') as out_f, \
-                    gzip.GzipFile(file_path) as zip_f:
-                out_f.write(zip_f.read())
-            os.unlink(file_path)
-
-        # process and save as torch files
-        print('Processing...')
-
-        training_set = (
-            read_image_file(os.path.join(self.root, self.raw_folder, 'train-images-idx3-ubyte')),
-            read_label_file(os.path.join(self.root, self.raw_folder, 'train-labels-idx1-ubyte'))
-        )
-        test_set = (
-            read_image_file(os.path.join(self.root, self.raw_folder, 't10k-images-idx3-ubyte')),
-            read_label_file(os.path.join(self.root, self.raw_folder, 't10k-labels-idx1-ubyte'))
-        )
-        with open(os.path.join(self.root, self.processed_folder, self.training_file), 'wb') as f:
-            torch.save(training_set, f)
-        with open(os.path.join(self.root, self.processed_folder, self.test_file), 'wb') as f:
-            torch.save(test_set, f)
-
-        print('Done!')
-
-    def __repr__(self):
-        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
-        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
-        tmp = 'train' if self.train is True else 'test'
-        fmt_str += '    Split: {}\n'.format(tmp)
-        fmt_str += '    Root Location: {}\n'.format(self.root)
-        tmp = '    Transforms (if any): '
-        fmt_str += '{0}{1}\n'.format(tmp, self.transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
-        tmp = '    Target Transforms (if any): '
-        fmt_str += '{0}{1}'.format(tmp, self.target_transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
-        return fmt_str
 
 def get_int(b):
     return int(codecs.encode(b, 'hex'), 16)
