33b457fc37f136ad406eacffd748d1222c5b993f
John Lalor <john.lalor@nd.edu>
2022-05-12 10:08:37 -0400

    a number of updates

--- src/cltb/cli.py
+++ src/cltb/cli.py
@@ -7,6 +7,11 @@ import typer
 import toml
 from rich.console import Console
 
+from transformers import AutoModelForSequenceClassification
+from transformers import TrainingArguments, Trainer
+import numpy as np
+from datasets import load_dataset, load_metric
+    
 from models.models import MODELS
 from datasets.datasets import DATASETS 
 from trainers.trainers import TRAINERS
@@ -24,20 +29,15 @@ def train(
         experiment_config = toml.load(f)
 
     # load data set
-    data = DATASETS[
-        experiment_config["data"]["name"]
-    ](experiment_config)
+    dataset = load_dataset(experiment_config["data"]["name"])
 
-    # check
-    if experiment_config["data"]["taskname"] == "MNLI":
-        dev_file = "dev_matched"
-    else:
-        dev_file = "dev"
+    # assign ids (always do this for consistency)
+    dataset = dataset.map(lambda x, idx: {"idx": idx}, with_indices=True) 
 
-    dev_data = DATASETS[
-        experiment_config["data"]["name"]
-    ](experiment_config, dev_file)
-
+    # load initial difficulties
+    difficulties = load_difficulties(experiment_config["data"]["diff_file"])
+    dataset = dataset.map(lambda x: {"difficulty": difficulties[x["idx"]]}) 
+    
     # load and initialize model
     model = MODELS[
         experiment_config["model"]["name"]
@@ -49,7 +49,7 @@ def train(
     ](experiment_config)
 
     # load the teacher 
-    teacher = DefaultTeacher(model, data, dev_data, trainer, experiment_config)
+    teacher = DefaultTeacher(model, data, trainer, experiment_config)
 
     # train model 
     teacher.train() 
@@ -59,6 +59,56 @@ def test(
     config_path: str
 ):
     pass
+
+@app.command
+def run_crowd(
+    config_path: str
+):
+    with open(config_path, "r") as f:
+        experiment_config = toml.load(f)
+
+    # load data set and assign ids
+    dataset = load_dataset(experiment_config["data"]["name"])
+    dataset = dataset.map(lambda x, idx: {"idx": idx}, with_indices=True)
+
+    # format dataset
+    model_name = experiment_config["model"]["name"]
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    def tokenize_function(examples):
+        return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+    tokenized_dataset = dataset.map(tokenize_function, batched=True)
+    train_dataset = tokenized_datasets["train"].shuffle(seed=42)
+    eval_dataset = tokenized_datasets["test"].shuffle(seed=42)
+
+    metric = load_metric("accuracy")
+
+    def compute_metrics(eval_pred):
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return metric.compute(predictions=predictions, references=labels)
+
+
+    crowd_size = experiment_config["model"]["crowd_size"]
+    for i in range(crowd_size):
+        model = AutoModelForSequenceClassification.from_pretrained(
+            model_name,
+            num_labels=len(set(train_dataset["label"]))
+        )
+        
+        training_args = TrainingArguments(
+            output_dir=f"test_trainer_{i}",
+            evaluation_strategy="epoch"
+        )
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset,
+            compute_metrics=compute_metrics,
+        )
+        trainer.train()
+
 
 if __name__ == "__main__":
     app()
